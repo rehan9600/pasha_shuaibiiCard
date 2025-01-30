b1da8e5cf0ede58af813a0576ce036013cd2c0ad
alrman_ali <aliarman9600@gmail.com>
2025-01-30 11:54:40 +0530

    update thecode

---
+++ docs/shuaibiicard/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-KNUZL2GJ.mjs";import"./chunk-R4A7OTKQ.mjs";import"./chunk-5VPKPZ3T.mjs";import"./chunk-KB5IWD3E.mjs";import"./chunk-EXD34EPT.mjs";export{a as default};
