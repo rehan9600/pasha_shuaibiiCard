f6efb46b6d4a1989d4a80939b3b5aab6e8cf99e0
Rehan-pasha <rehanpasha7830@gmail.com>
2025-08-11 11:56:40 +0530

    Fix: Added .nojekyll and corrected output path

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -3,7 +3,7 @@
  */
 
 // Netlify Edge Functions fix for `globalThis is not defined`
-(window as any).globalThis = window;
+(window as any).global = window;
 (window as any).process = { env: {} };
 (window as any).Buffer = [];
 
