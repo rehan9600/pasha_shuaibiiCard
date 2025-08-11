ecdf17e78a2c4d1fc91a3f480a8bd84676156f3f
Rehan-pasha <rehanpasha7830@gmail.com>
2025-08-11 10:43:08 +0530

    Netlify deploy fix

--- docs/shuaibiicard/server/main.server.mjs
+++ docs/shuaibiicard/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-KNUZL2GJ.mjs";import"./chunk-R4A7OTKQ.mjs";import"./chunk-5VPKPZ3T.mjs";import"./chunk-KB5IWD3E.mjs";import"./chunk-EXD34EPT.mjs";export{a as default};
+import{a}from"./chunk-Y6RHBZVN.mjs";import"./chunk-LGRAUEW5.mjs";import"./chunk-5VPKPZ3T.mjs";import"./chunk-KB5IWD3E.mjs";import"./chunk-EXD34EPT.mjs";export{a as default};
